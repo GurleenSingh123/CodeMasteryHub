cf87d70b53e62ddefca68069d86a240ecb024246
Gurleen Singh <gurleen@gmail.com>
2026-04-19 23:57:03 +0530

    file updated

--- btn.js
+++ btn.js
@@ -27,8 +27,8 @@ function setupNavigation(){
     "inputcontrol.html", 
     "formusability.html", 
     "formexample.html", 
-    "semantichtml.html"
-    // "navigation.html"
+    "semantichtml.html",
+    "navigation.html"
   ];
 
   let current = window.location.pathname.split("/").pop();
